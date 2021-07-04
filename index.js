fa2ecd8e84579ee3dc5ee5a8769d6521c6457958
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2021-07-05 00:48:05 +0500

    First javascript variables

---
+++ index.js
@@ -0,0 +1,18 @@
+alert("Universitetga hujjat topshirish uchun so`rovnomaga hush kebsiz  :)")
+var firstName = prompt("Iltimos ismingizni kiriting: ", 'Boltaboy');
+var lasttName = prompt("Iltimos familyangizni kiriting: ", "Teshaboyev");
+var fathersName = prompt("Iltimos otangizni ismini kiriting: ", "Ketmonovich");
+var address = prompt("Iltimos manzilingizni kiriting: ", "Kesak topdi qishlog`i");
+var age = prompt("Iltimos yoshingizni kiriting: ", "19");
+var education = prompt("Litsey yoki kollejingiz ismi: ", "Temir yo`llari kolleji");
+var educationYear = prompt("Litsey yoki kollejingiz tomonlagan yilingiz: ", "2020");
+var passport = prompt("Passport raqamingiz: ", "AA0000000");
+var choosenField = prompt("Qaysi yo`nalishni tanlamoqchisiz: ", "IT/Ekonomika/Med/jismoniy-tarbiya");
+var foundationOption = prompt("Boshlangich bilim-ko`nikma omoqchimisiz: ", "Ha/Yo`q");
+
+
+console.log('ism: ' + firstName + '\nfamiliya: ' + lasttName + '\nOtangizni ismi: ' + fathersName + '\nmanzil: ' + address + '\nyosh: ' + age + '\ntomomlagan o`qish joy: ' + education + '\no`qishni tomomlagan yil: ' + educationYear + 'passport: ' + passport + '\nTanlagan yo`nalishingiz: ' + choosenField + '\nBoshlang`ich bilim ko`nikmaga roziligingiz: ' + foundationOption);
+
+alert("So`rovnomani muvaffaqiyatli yakunladingiz!")
+alert("Vaqtingiz va e`tiboringiz uchun tashakkur!!!")
+
